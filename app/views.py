67720723fcc0c0dd39389314c72c26a957f8d258
spw <spw@spws-MacBook-Pro.local>
2016-02-11 03:56:24 -0800

    subtract 1 for zero index

--- app/views.py
+++ app/views.py
@@ -51,7 +51,7 @@ def query(request):
     if under_300 or exclude_CS:
       continue
     scores[to_code] = score
-  results = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+  results = sorted(scores.items(), key=lambda x: x[1], reverse=True)[-20:]
   ### DONE: PCA ###
 
   res = {}
