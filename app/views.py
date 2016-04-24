541d44a7d05e4ccc7badca4ca84b177d7ebc4315
spw <spw@spws-MacBook-Pro.local>
2016-04-24 07:10:14 -0700

    turn on freq counting, turn off pca

--- app/views.py
+++ app/views.py
@@ -15,44 +15,24 @@ def query(request):
   number = request.GET.get('number')
   excl = int(request.GET.get('excl'))
 
-  ### PCA ###
-  scores = {}
-  # get key for given course
-  code = course + str(number)
-  # course_key = Course.objects.filter(code__startswith=code).first().unique_id - 1
-  course_key = -1
-  keys = {}
-  for c in Course.objects.all():
-    c_key = c.unique_id
-    c_code = c.code
-    if c_code.startswith(code):
-      # subtract 1 since Similarity table is zero indexed
-      course_key = c_key - 1
-    keys[c_key] = c_code
-  if course_key < 0:
-    res = {}
-    res['course'] = course
-    res['number'] = number
-    res['results'] = [-1]
-    return HttpResponse(json.dumps(res))
-  # create a query set
-  sims = Similarity.objects.filter(from_class=course_key).exclude(to_class=course_key)
-  regex = re.compile(r'[^\d]+')
-  for sim in sims:
-    score = sim.score
-    # add 1 back since Course table is not zero indexed
-    to_unique_id = sim.to_class + 1
-    # Load Course key table in array instead
-    # to_code = Course.objects.filter(unique_id=to_unique_id).first().code
-    to_code = keys[to_unique_id]
-    to_number = int(regex.sub('', to_code))
-    under_300 = to_number < 300
-    exclude_CS = excl and to_code.startswith('CPSC')
-    if under_300 or exclude_CS:
-      continue
-    scores[to_code] = score
-  results = sorted(scores.items(), key=lambda x: x[1], reverse=True)[:40]
-  ### DONE: PCA ###
+  ### Frequency counting ###
+  counts = {}
+  # all student ids from enrolments with given class
+  givenClassIds = [enrolment.id for enrolment in Enrolment.objects.filter(dept=course).filter(number=number).filter(number__gte=300)]
+  # for each student who took the given class
+  for id in givenClassIds:
+    # create a query set of other classes that student took
+    classesWith = Enrolment.objects.exclude(dept=course, number=number).filter(id=id).filter(number__gte=300)
+    # keep count of these classes
+    for c in classesWith:
+      name = c.dept + str(c.number)
+      if name in counts.keys():
+        counts[name] += 1
+      else:
+        counts[name] = 1
+  # sort by descending count
+  results = sorted(counts.items(), key=lambda x: x[1], reverse=True)
+  ### DONE: Frequency counting ###
 
   res = {}
   res['course'] = course
@@ -60,21 +40,41 @@ def query(request):
   res['results'] = results
   return HttpResponse(json.dumps(res))
 
-  ### Frequency counting ###
-  # counts = {}
-  # # all student ids from enrolments with given class
-  # givenClassIds = [enrolment.id for enrolment in Enrolment.objects.filter(dept=course).filter(number=number).filter(number__gte=300)]
-  # # for each student who took the given class
-  # for id in givenClassIds:
-  #   # create a query set of other classes that student took
-  #   classesWith = Enrolment.objects.exclude(dept=course, number=number).filter(id=id).filter(number__gte=300)
-  #   # keep count of these classes
-  #   for c in classesWith:
-  #     name = c.dept + str(c.number)
-  #     if name in counts.keys():
-  #       counts[name] += 1
-  #     else:
-  #       counts[name] = 1
-  # # sort by descending count
-  # results = sorted(counts.items(), key=lambda x: x[1], reverse=True)
-  ### DONE: Frequency counting ###
+  ### PCA ###
+  # scores = {}
+  # # get key for given course
+  # code = course + str(number)
+  # # course_key = Course.objects.filter(code__startswith=code).first().unique_id - 1
+  # course_key = -1
+  # keys = {}
+  # for c in Course.objects.all():
+  #   c_key = c.unique_id
+  #   c_code = c.code
+  #   if c_code.startswith(code):
+  #     # subtract 1 since Similarity table is zero indexed
+  #     course_key = c_key - 1
+  #   keys[c_key] = c_code
+  # if course_key < 0:
+  #   res = {}
+  #   res['course'] = course
+  #   res['number'] = number
+  #   res['results'] = [-1]
+  #   return HttpResponse(json.dumps(res))
+  # # create a query set
+  # sims = Similarity.objects.filter(from_class=course_key).exclude(to_class=course_key)
+  # regex = re.compile(r'[^\d]+')
+  # for sim in sims:
+  #   score = sim.score
+  #   # add 1 back since Course table is not zero indexed
+  #   to_unique_id = sim.to_class + 1
+  #   # Load Course key table in array instead
+  #   # to_code = Course.objects.filter(unique_id=to_unique_id).first().code
+  #   to_code = keys[to_unique_id]
+  #   to_number = int(regex.sub('', to_code))
+  #   under_300 = to_number < 300
+  #   exclude_CS = excl and to_code.startswith('CPSC')
+  #   if under_300 or exclude_CS:
+  #     continue
+  #   scores[to_code] = score
+  # results = sorted(scores.items(), key=lambda x: x[1], reverse=True)[:40]
+  ### DONE: PCA ###
